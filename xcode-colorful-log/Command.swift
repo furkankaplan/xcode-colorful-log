d5e8fd564f194b185cddb17f6b6f5404aac8214b
Furkan Kaplan <furkankaplan@Furkans-MacBook-Pro.local>
2022-01-08 16:48:45 +0300

    Get capable of running shell command

---
+++ xcode-colorful-log/Command.swift
@@ -0,0 +1,58 @@
+//
+//  Command.swift
+//  xcode-colorful-log
+//
+//  Created by Furkan Kaplan on 8.01.2022.
+//
+
+import Foundation
+
+protocol CommandProtocol {
+    func run()
+}
+
+protocol CommandDelegate: AnyObject {
+    func command(_ message: String)
+}
+
+class Command: CommandProtocol {
+    
+    private let process = Process()
+    private let pipe = Pipe()
+    private let launchPath = "/usr/bin/env"
+    private let arguments: [String] = ["bash", "-c"]
+    private let command: String = "log stream --debug --info"
+    
+    private var handler: FileHandle!
+    private var key: String!
+    
+    private weak var delegate: CommandDelegate!
+    
+    private init() { /* Not Allowed */ }
+    
+    init(key: String, delegate: CommandDelegate) {
+        self.key = key
+        
+        self.process.launchPath = self.launchPath
+        self.process.arguments = self.arguments + [command]
+        self.process.standardOutput = pipe
+        
+        self.handler = pipe.fileHandleForReading
+        
+        self.delegate = delegate
+    }
+    
+    func run() {
+        handler.readabilityHandler = { [weak self] pipe in
+            guard let self = self else { return }
+            guard let line = String(data: pipe.availableData, encoding: .utf8) else { return }
+            guard line.contains(self.key) else { return }
+            
+            self.delegate?.command(line)
+        }
+        
+        process.launch()
+        process.waitUntilExit()
+    }
+    
+}
