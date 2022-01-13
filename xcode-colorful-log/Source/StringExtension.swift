3e6c733d7476d6ed4f4a376d5d09e1521de20c51
Furkan Kaplan <furkankaplan@Furkans-MacBook-Pro.local>
2022-01-13 22:20:10 +0300

    Add source code for adding to the projects being logged

---
+++ xcode-colorful-log/Source/StringExtension.swift
@@ -0,0 +1,16 @@
+//
+//  StringExtension.swift
+//  xcode-colorful-log
+//
+//  Created by Furkan Kaplan on 13.01.2022.
+//
+
+import Foundation
+
+/// Logs collected by the system is not completely in the regular format. It's not line by line all the time.
+/// Because of that, encoding is required to parse logs correctly.
+extension String {
+    
+    var encode: String { self.replacingOccurrences(of: "\n", with: "@new-line@").replacingOccurrences(of: " ", with: "@space@") }
+    
+}
